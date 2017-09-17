b9631f8683437fefc617fbdd9615fa6d8187b191
micah <micah@micahs-MacBook-Pro.local>
2017-09-16 14:22:46 -1000

    ESLint was improperly configured and gave an OK even though it wasn't working correctly.

--- ziplist.js
+++ ziplist.js
@@ -1,16 +1,17 @@
 function zipList(arr1, arr2) {
-	if (arr1.length != arr2.length) return null;
-	let length = arr1.length;
-	let newarr = [];
-	for (let i = 0; i < length; i++) {
-		newarr.push(arr1[i]);
-		newarr.push(arr2[i]);
-	}
-	return newarr;
-}
-function zipListTheSimpleWay(arr1, arr2) {
-	return _.flatten(_.zip(arr1, arr2));
+  if (arr1.length !== arr2.length) return null;
+  const { length } = arr1;
+  const newarr = [];
+  for (let i = 0; i < length; i++) {
+    newarr.push(arr1[i]);
+    newarr.push(arr2[i]);
+  }
+  return newarr;
 }
 
-console.log(zipList(['a','b','c'],[1,2,3]));
-console.log(zipListTheSimpleWay(['a','b','c'],[1,2,3]));
+function zipListTheSimpleWay(arr1, arr2) {
+  return _.flatten(_.zip(arr1, arr2));
+}
+
+console.log(zipList(['a', 'b', 'c'], [1, 2, 3]));
+console.log(zipListTheSimpleWay(['a', 'b', 'c'], [1, 2, 3]));
